4e6d95fccaa3b1bdc71aa90b0403449cc9f1fcb8
77infinite <123508309+77infinite@users.noreply.github.com>
2023-06-01 13:40:42 -0300

    Update Energia mecanica.py

--- Energia mecanica.py
+++ Energia mecanica.py
@@ -3,27 +3,27 @@ print("Bem vindo ao app de energia mecanica!")
 print("1 - Massa")
 print("2 - Altura")
 print("3 - Velocidade")
-opt = int(input("Digite qual tipo de conta deseja: "))
+opt = float(input("Digite qual tipo de conta deseja: "))
 #descobrir a massa
 if (opt == 1):
-    J = float("Digite a quantidade de Jaws: ")
-    G = float("Digite a gravidade: ")
-    H = float("Digite a altura: ")
+    J = float(input("Digite a quantidade de Jaws: "))
+    G = float(input("Digite a gravidade: "))
+    H = float(input("Digite a altura: "))
     M1 = G * H
     Mf = J/M1
     print("A massa é igual a ", Mf)
 #descobrir a altura
 elif (opt == 2):
-    J = float("Digite a quantidade de Jaws: ")
-    G = float("Digite a gravidade: ")
-    M = float("Digite a massa: ")
+    J = float(input("Digite a quantidade de Jaws: "))
+    G = float(input("Digite a gravidade: "))
+    M = float(input("Digite a massa: "))
     H1 = G*M
     Hf = J/H1
     print("A altura é igual a ", Hf)
 #descobrir a velocidade
 elif (opt == 3):
-    J = float("Digite a quantidade de Jaws: ")
-    M = float("Digite a massa: ")
+    J = float(input("Digite a quantidade de Jaws: "))
+    M = float(input("Digite a massa: "))
     V1 = J*2
     V2 = J/M
     Vf = J^0.5
